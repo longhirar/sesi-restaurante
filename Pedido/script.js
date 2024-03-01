7c6e8d08ec05388f6a6c0248461560a4884880b9
longhirar <longhirar@gmail.com>
2024-03-01 11:36:56 -0300

    01-03

--- Pedido/script.js
+++ Pedido/script.js
@@ -1,4 +1,22 @@
 
+const CARDAPIO = [
+    [ // Pratos
+        14.90, // Prato do Dia
+        19.90, // Macarronofe
+        22.90  // Lasanha
+    ],
+    [ // Bebidas
+        5.99, // Refri
+        6.99, // Suco de Laranja
+        3.50  // Água
+    ],
+    [ // Sobremesas
+        9.99, // Sorvete
+        6.99, // Pudim
+        9.99  // Torta de Limão
+    ]
+]
+
 function modalErrorClose() {
     document.querySelector('#error').style.display = 'none';
     document.querySelector('#error').animate("spinShow")
@@ -24,4 +42,32 @@ function comecar() {
     } else {
         modalError("Você precisa informar o seu nome!");
     }
+}
+
+function limparSelecao() {
+    document.querySelectorAll('.form-select').forEach(select => {
+        select.selectedIndex = 0;
+    });
+    calcularTotal()
+}
+
+function calcularTotal() {
+    var total = 0.00;
+    var selects = document.querySelectorAll('.form-select')
+    for (let i = 0; i < selects.length; i++) {
+        var select = selects[i]
+        var selecionado = select.selectedIndex;
+        
+        if (selecionado != 0) {
+            total += CARDAPIO[i][selecionado-1];
+        }
+    }
+
+    document.querySelector("#price-display").textContent = `R$ ${total.toFixed(2)}`
+    return total
+}
+
+function fazerPedido() {
+    
+    swal("Pedido Realizado!", `Preço: R$ ${calcularTotal().toFixed(2)}`, "success");
 }
